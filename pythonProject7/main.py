2fcbaa4ee34735c0a6459cb8d5f2ad499d475cd5
VLADOSDUDOS <146865995+VLADOSDUDOS@users.noreply.github.com>
2023-12-02 11:52:44 +0300

    Update main.py
    исправил прогграмму

--- pythonProject7/main.py
+++ pythonProject7/main.py
@@ -1,33 +1,17 @@
-def check_and_print_binary_multiple_of_3(lines, c1):
-    i = 0
+import re
 
-    decimal_number = 0
-    length = len(line)
+def find_binary_multiple_of_3(lines):
+    pattern = re.compile(r'^0*(1(01*0)*1|0*)*$')  # Регулярное выражение для чисел, кратных 3 в двоичной системе
 
-    while i < length:
-        s_int = ''  # строка для нового числа
-        while (i < length) and ('0' <= line[i] <= '1'):
-            s_int += line[i]
-            i += 1
-        i += 1
+    for line in lines:
+        if pattern.match(line):
+            print(f"Найдено число в двоичной записи, кратное 3: {line}")
 
-        if s_int != '':
-            decimal_number = int(s_int, 2)
-            if decimal_number % 3 == 0:
-                c1 += 1
-    return c1
-
-
-# Пользовательский ввод
+# Пример использования
 n = int(input("Введите количество строк: "))
 user_lines = []
 
-c = 0
 for _ in range(n):
     line = input("Введите строку: ")
     user_lines.append(line)
-    c = check_and_print_binary_multiple_of_3(user_lines, c)
-    if c != 0:
-        print(f"Найдено число в двоичной записи, кратное 3, в строке: {line}")
-    else:
-        print(f"Чисел в двоичной записи, кратных 3, в строке: {line} не найдено")
+find_binary_multiple_of_3(user_lines)
